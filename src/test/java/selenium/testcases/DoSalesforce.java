d3fed8498e3df87e73197b4198ec08076df5f3dd
João Paulo de Lima <jpofl2@gmail.com>
2018-10-28 17:30:42 -0300

    Update DoSalesforce.java

--- src/test/java/selenium/testcases/DoSalesforce.java
+++ src/test/java/selenium/testcases/DoSalesforce.java
@@ -23,32 +23,35 @@ import selenium.utils.annotations.browser.BrowserDimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
 
+import io.github.bonigarcia.wdm.PhantomJsDriverManager;
+
 @BrowserDimension(XLARGE)
 @Browser(skip = { INTERNET_EXPLORER, EDGE, PHANTOMJS })
 public class DoSalesforce extends SeleniumTestWrapper {
 
 	@Before
-	public void setup() {}
+	public void setup() {
+		PhantomJsDriverManager.getInstance().setup("2.1.1");
+	}
 
 	@Test
 	public void verifyCreateLead() {
-    WebDriver driver = getDriver();
+		
+		WebDriver driver = getDriver();
 
-    driver.get("https://login.salesforce.com/");
-    driver.findElement(By.id("username")).clear();
-    driver.findElement(By.id("username")).sendKeys("jpofl2@gmail.com");
-    driver.findElement(By.id("password")).clear();
-    driver.findElement(By.id("password")).sendKeys("1qaz2wsx");
-    driver.findElement(By.id("Login")).click();
-    driver.findElement(By.linkText("Leads")).click();
-    driver.findElement(By.name("new")).click();
-    driver.findElement(By.id("name_lastlea2")).click();
-    driver.findElement(By.id("name_lastlea2")).clear();
-    driver.findElement(By.id("name_lastlea2")).sendKeys("Skywalker");
-    driver.findElement(By.id("lea3")).clear();
-    driver.findElement(By.id("lea3")).sendKeys("Jedi");
-    driver.findElement(By.name("save")).click();
-
-
+		driver.get("https://login.salesforce.com/");
+		driver.findElement(By.id("username")).clear();
+		driver.findElement(By.id("username")).sendKeys("jpofl2@gmail.com");
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("password")).sendKeys("1qaz2wsx");
+		driver.findElement(By.id("Login")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.name("new")).click();
+		driver.findElement(By.id("name_lastlea2")).click();
+		driver.findElement(By.id("name_lastlea2")).clear();
+		driver.findElement(By.id("name_lastlea2")).sendKeys("Skywalker");
+		driver.findElement(By.id("lea3")).clear();
+		driver.findElement(By.id("lea3")).sendKeys("Jedi");
+		driver.findElement(By.name("save")).click();
 	}
 }
